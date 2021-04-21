de45e1deb972a29b2027a04520cc5c3ddd62bd10
Luca <wachters.luca@gmail.com>
2021-04-21 21:58:04 +0200

    init Game class

--- GameProject/Game.cpp
+++ GameProject/Game.cpp
@@ -1,10 +1,89 @@
-#include "Game.h"
+#include "game.h"
 
-// constructors / destructors
+//private functions
+void Game::initVariables()
+{
+	this->window = nullptr;
+}
+
+void Game::initWindow()
+{
+	this->videoMode.height = 480;
+	this->videoMode.width = 720;
+	this->window = new sf::RenderWindow(this->videoMode, "GameProject", sf::Style::Titlebar | sf::Style::Close);
+}
+
+//constructors / destructors
 Game::Game()
 {
+	this->initVariables();
+	this->initWindow();
 }
 
 Game::~Game()
 {
+	delete this->window;
+}
+
+//accessors
+const bool Game::running() const
+{
+	return this->window->isOpen();
+}
+
+
+
+//functions
+void Game::pollEvents()
+{
+	//Event Polling
+	while (this->window->pollEvent(this->ev))
+	{
+		switch (this->ev.type)
+		{
+		case sf::Event::Closed:
+			this->window->close();
+			break;
+		case sf::Event::KeyPressed:
+			switch (this->ev.key.code)
+			{
+			case sf::Keyboard::Escape:
+				this->window->close();
+				break;
+			}
+			break;
+		}
+	}
+}
+
+void Game::update()
+{
+	//poll events
+	this->pollEvents();
+}
+
+void Game::render()
+{
+	//clear old frame
+	this->window->clear(sf::Color::Cyan);
+
+	//render items
+
+	//display frame in window
+	this->window->display();
+}
+
+void Game::run()
+{
+	//Game Loop
+	while (this->running())
+	{
+
+		//update
+		this->update();
+
+		//render
+		this->render();
+
+	}
 }
--- GameProject/Game.h
+++ GameProject/Game.h
@@ -1,11 +1,36 @@
 #pragma once
+
+#include <SFML\Graphics.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Audio.hpp>
+#include <SFML\Network.hpp>
+
 class Game
 {
 private:
+	//variables
+	//window
+	sf::RenderWindow* window;
+	sf::Event ev;
+	sf::VideoMode videoMode;
+
+	//private functions
+	void initVariables();
+	void initWindow();
 
 public:
 	//constructors / destructors
 	Game();
 	~Game();
+
+	//accessors
+	const bool running() const;
+
+	//functions
+	void pollEvents();
+	void update();
+	void render();
+	void run();
 };
 

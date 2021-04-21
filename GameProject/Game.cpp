5cd21383eeea720b2d290eb809278df9a000d091
Luca <wachters.luca@gmail.com>
2021-04-21 21:12:22 +0200

    Add project files.

---
+++ GameProject/Game.cpp
@@ -0,0 +1,10 @@
+#include "Game.h"
+
+// constructors / destructors
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
---
+++ GameProject/Game.h
@@ -0,0 +1,11 @@
+#pragma once
+class Game
+{
+private:
+
+public:
+	//constructors / destructors
+	Game();
+	~Game();
+};
+
